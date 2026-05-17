f731f6ab3b8cc73efca5448162f1f93b36164feb
sats17 <satishkumbhar9999@gmail.com>
2026-05-17 10:05:22 +0530

    Updated base url

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,7 +3,7 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	url: "https://sats17.github.io",
-	base: "/sats17.blogs",
+	base: "/blogs",
 	/*
 		- Used to construct the meta title property found in src/components/BaseHead.astro L:11
 		- Used to construct the meta title property found in src/components/BaseHead.astro
